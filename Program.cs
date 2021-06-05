ae72e324f695f86be650724114ff19a9f85b902f
Marcelo Carvalho <marcelo.scientist@hotmail.com>
2021-06-05 12:58:16 -0300

    Learning a little about Repositories or in Portuguese 'Repositorios'

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+            while (opcaoUsuario.ToUpper() != "X") {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                    ListarSeries();
+                        break;
+                    case "2":
+                    InserirSerie();
+                        break;
+                    case "3":
+                    //AtualizarSerie();
+                        break;
+                    case "4":
+                    //ExcluirSerie();
+                        break;
+                    case "5":
+                    //VisualizarSerie();
+                        break;
+                    case "C":
+                    Console.Clear();
+                        break;   
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                        //break;
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            
+            Console.WriteLine("Obrigado por utilizar nossos serviços");
+            Console.ReadLine();
+        }
+
+        private static void ListarSeries() {
+            Console.WriteLine("Listar séries");
+            var lista = repositorio.Lista();
+            if(lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+            foreach (var serie in lista)
+            {
+                Console.WriteLine("#ID {0}: - {1}", serie.retornaId(), serie.retornaTitulo());
+            }
+
+        }
+       
+        private static void InserirSerie() {
+            Console.WriteLine("Inserir nova séries");
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.Write("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+            
+            Console.Write("Digite o Título da série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano de Início da série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descriçao da Série: ");
+            string entradaDescricao = Console.ReadLine();
+            
+            Serie novaSerie = new Serie(id: repositorio.ProximoId(),
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo, 
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+            repositorio.Insere(novaSerie);
+        }
+
+        private static string ObterOpcaoUsuario() {
+            Console.WriteLine();
+            Console.WriteLine("DIO Séries a seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada: ");
+
+            Console.WriteLine("1- Listar séries");
+            Console.WriteLine("2- Inserir nova série");
+            Console.WriteLine("3- Atualizar série");
+            Console.WriteLine("4- Excluir série");
+            Console.WriteLine("5- Visualizar série");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string ObterOpcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return ObterOpcaoUsuario;
+
+        }
+
+    }
+}
